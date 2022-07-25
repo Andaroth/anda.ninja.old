ac6c6fd1c914bf8a47829cc0e3006a74cd999dec
Anda <ax.fiolle@gmail.com>
2022-07-25 18:41:15 +0200

    Footer : add Git link + update tech in credits

--- app/core/components/Footer.tsx
+++ app/core/components/Footer.tsx
@@ -1,13 +1,19 @@
 import { Flex, Box, Text } from "@chakra-ui/react"
-import { Code } from "@chakra-ui/react"
+import { Code, Link } from "@chakra-ui/react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
 
 export const Footer = () => {
   return (
     <Flex p="8" bg="#2e3133" borderTop="2px solid #cc9a54">
       <Box color="white" margin="0 auto" textAlign="center">
-        <Text mb="4">
-          Made with <Code>Blitz</Code>, <Code>ReactJS</Code>, <strong>Love</strong> and lot of{" "}
+        <Text>
+          Made with <Code>ReactJS</Code>, <Code>ChakraUI</Code>, <strong>Love</strong> and{" "}
           <Code>!patience</Code>.
+        </Text>
+        <Text mt="1" mb="4">
+          <Link href="https://github.com/Andaroth/anda.ninja" target="_blank">
+            View sourcecode on GitHub <ExternalLinkIcon mx="2px" />
+          </Link>
         </Text>
         <Text>Axel Fiolle 2005 - {new Date().getFullYear()} &copy; All rights reserved</Text>
       </Box>
