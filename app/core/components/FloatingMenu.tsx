d214d9d46bf96d9e4bc11beb9aa31b98d71d8bf0
Anda <ax.fiolle@gmail.com>
2022-07-26 01:34:06 +0200

    Improve html5 tag

--- app/core/components/FloatingMenu.tsx
+++ app/core/components/FloatingMenu.tsx
@@ -10,93 +10,97 @@ interface Props {
 
 export const FloatingMenu: FC<Props> = ({ page = "home" }: Props) => {
   return (
-    <Flex
-      p="0"
-      position="fixed"
-      top="0"
-      left="0"
-      bg="rgba(0,0,0,.7)"
-      color="white"
-      w="100%"
-      overflowX="auto"
-    >
-      <Link to="top" smooth={true}>
-        <Heading
-          as="h1"
-          cursor="pointer"
-          m="2"
-          ml="4"
-          css={`
-            &:hover {
-              color: #cc9a54;
-            }
-          `}
-        >
-          <Text display="none">
-            <span className="nomobile">&nbsp;Axel&nbsp;</span>Andaroth
-          </Text>
-          <Text fontFamily="UbuntuL">![Λnda]</Text>
-        </Heading>
-      </Link>
-      <Spacer w="100%" />
-      {page === "home" && (
-        <Flex fontFamily="Ubuntu">
-          <Link to="about" smooth={true} className="nomobile">
-            <Text
-              fontSize="2xl"
-              p="2"
-              mr="2"
-              cursor="pointer"
-              h="100%"
-              css={`
-                &:hover {
-                  color: #cc9a54;
-                  border-bottom: 4px solid #cc9a54;
-                }
-              `}
-            >
-              About me
+    <header>
+      <Flex
+        p="0"
+        position="fixed"
+        top="0"
+        left="0"
+        bg="rgba(0,0,0,.7)"
+        color="white"
+        w="100%"
+        overflowX="auto"
+      >
+        <Link to="top" smooth={true}>
+          <Heading
+            as="h1"
+            cursor="pointer"
+            m="2"
+            ml="4"
+            css={`
+              &:hover {
+                color: #cc9a54;
+              }
+            `}
+          >
+            <Text display="none">
+              <span className="nomobile">&nbsp;Axel&nbsp;</span>Andaroth
             </Text>
-          </Link>
-          <Link to="work" smooth={true} className="nomobile">
-            <Text
-              fontSize="2xl"
-              p="2"
-              mr="2"
-              cursor="pointer"
-              h="100%"
-              css={`
-                &:hover {
-                  color: #cc9a54;
-                  border-bottom: 4px solid #cc9a54;
-                }
-              `}
-            >
-              My work
-            </Text>
-          </Link>
-          <a href="mailto:ax.fiolle@gmail.com">
-            <Button
-              className="detail small"
-              mr="4"
-              cursor="pointer"
-              mt="2"
-              textTransform="uppercase"
-              css={`
-                padding: 0;
-                font-family: "UbuntuB";
-                &:hover {
-                  color: #cc9a54;
-                  border-bottom: 4px solid #cc9a54;
-                }
-              `}
-            >
-              Contact
-            </Button>
-          </a>
-        </Flex>
-      )}
-    </Flex>
+            <Text fontFamily="UbuntuL">![Λnda]</Text>
+          </Heading>
+        </Link>
+        <Spacer w="100%" />
+        {page === "home" && (
+          <nav>
+            <Flex fontFamily="Ubuntu">
+              <Link to="about" smooth={true} className="nomobile">
+                <Text
+                  fontSize="2xl"
+                  p="2"
+                  mr="2"
+                  cursor="pointer"
+                  h="100%"
+                  css={`
+                    &:hover {
+                      color: #cc9a54;
+                      border-bottom: 4px solid #cc9a54;
+                    }
+                  `}
+                >
+                  About me
+                </Text>
+              </Link>
+              <Link to="work" smooth={true} className="nomobile">
+                <Text
+                  fontSize="2xl"
+                  p="2"
+                  mr="2"
+                  cursor="pointer"
+                  h="100%"
+                  css={`
+                    &:hover {
+                      color: #cc9a54;
+                      border-bottom: 4px solid #cc9a54;
+                    }
+                  `}
+                >
+                  My work
+                </Text>
+              </Link>
+              <a href="mailto:ax.fiolle@gmail.com">
+                <Button
+                  className="detail small"
+                  mr="4"
+                  cursor="pointer"
+                  mt="2"
+                  textTransform="uppercase"
+                  css={`
+                    padding: 0;
+                    font-family: "UbuntuB";
+                    &:hover {
+                      color: #cc9a54;
+                      border-bottom: 4px solid #cc9a54;
+                    }
+                  `}
+                >
+                  Contact
+                </Button>
+              </a>
+            </Flex>
+          </nav>
+        )}
+      </Flex>
+    </header>
   )
 }
 
--- app/core/components/Footer.tsx
+++ app/core/components/Footer.tsx
@@ -4,20 +4,22 @@ import { ExternalLinkIcon } from "@chakra-ui/icons"
 
 export const Footer = () => {
   return (
-    <Flex p="8" bg="#2e3133" borderTop="2px solid #cc9a54">
-      <Box color="white" margin="0 auto" textAlign="center">
-        <Text>
-          Made with <Code>ReactJS</Code>, <Code>ChakraUI</Code>, <strong>Love</strong> and{" "}
-          <Code>!patience</Code>.
-        </Text>
-        <Text mt="1" mb="4">
-          <Link href="https://github.com/Andaroth/anda.ninja" target="_blank">
-            View sourcecode on GitHub <ExternalLinkIcon mx="2px" />
-          </Link>
-        </Text>
-        <Text>Axel Fiolle 2005 - {new Date().getFullYear()} &copy; All rights reserved</Text>
-      </Box>
-    </Flex>
+    <footer>
+      <Flex p="8" bg="#2e3133" borderTop="2px solid #cc9a54">
+        <Box color="white" margin="0 auto" textAlign="center">
+          <Text>
+            Made with <Code>ReactJS</Code>, <Code>ChakraUI</Code>, <strong>Love</strong> and{" "}
+            <Code>!patience</Code>.
+          </Text>
+          <Text mt="1" mb="4">
+            <Link href="https://github.com/Andaroth/anda.ninja" target="_blank">
+              View sourcecode on GitHub <ExternalLinkIcon mx="2px" />
+            </Link>
+          </Text>
+          <Text>Axel Fiolle 2005 - {new Date().getFullYear()} &copy; All rights reserved</Text>
+        </Box>
+      </Flex>
+    </footer>
   )
 }
 
