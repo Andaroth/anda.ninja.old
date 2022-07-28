4be23a87b064afeee4e5ef5f1aa71dbfd7764b06
Anda <ax.fiolle@gmail.com>
2022-07-28 22:33:05 +0200

    Add Metadatas & OpenGraph

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -4,11 +4,28 @@ const Layout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
   title,
   children,
 }) => {
+  const description = "Creator of incredible interfaces to make the web a wonderful world."
   return (
     <>
       <Head>
-        <title>{title || "axel"}</title>
+        <title>{title || "Andaroth"}</title>
         <link rel="icon" href="./img/fav.gif" />
+        <meta property="og:image" content="./img/fav.gif" />
+        <meta property="og:title" content={title || "Andaroth"} />
+        <meta property="og:description" content={description} />
+        <meta name="description" content={description} />
+
+        <meta
+          name="keywords"
+          content="axel,fiolle,andaroth,blockchain,ethereum,crypto,btc,eth,photographie,photographe,illustration,graphisme,graphiste,infographie,infographiste,web,design,designer,webmaster,user,experience,interface,ui,ux,uxui,portfolio,infographie,responsive,jquery,js,javascript,css,html,php,mysql,maria,mariadb,ruby,computer,graphics"
+        />
+
+        <meta name="author" content="Axel Fiolle" />
+
+        <meta name="msapplication-tooltip" content={description} />
+        <meta name="msapplication-starturl" content="https://anda.ninja/" />
+
+        <meta name="wot-verification" content="b6e6901e016c39508cad" />
       </Head>
 
       {children}
